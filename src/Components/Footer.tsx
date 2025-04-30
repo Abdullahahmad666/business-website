1833577e9cc5077958a9b078e72f016c0bdfc3a3
Abdullah <l201326@lhr.nu.edu.pk>
2025-04-30 18:17:47 +0500

    email change

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -57,8 +57,8 @@ function Footer() {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                <a href="mailto:sohaibuppal65@gmail.com" className="footer-link">
-                  sohaibuppal65@gmail.com
+                <a href="mailto:info@asuppaltradinggmbh.com" className="footer-link">
+                info@asuppaltradinggmbh.com
                 </a>
               </p>
             </div>
--- src/Pages/Categories.tsx
+++ src/Pages/Categories.tsx
@@ -101,7 +101,7 @@ function CategoriesPage() {
                 d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
               />
             </svg>
-            <a href="mailto:sohaibuppal85@gmail.com">sohaibuppal85@gmail.com</a>
+            <a href="mailto:info@asuppaltradinggmbh.com">info@asuppaltradinggmbh.com</a>
           </div>
           <div className="top-bar-item space-x-2">
             <a href="#" aria-label="Twitter">
